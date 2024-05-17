741e45badbed32427fc07b158dd7a0a24d38cb8f
omar ogaili <ogailiomar@hotmail.com>
2024-05-17 11:29:44 +0200

    some changes

--- my_env/bin/activate.csh
+++ my_env/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/omar/Desktop/Desktop-omar/lia/test_env"
+setenv VIRTUAL_ENV "/home/omar/Desktop/Desktop-omar/Sesame2-sim/my_env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -18,8 +18,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(test_env) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(test_env) "
+    set prompt = "(my_env) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(my_env) "
 endif
 
 alias pydoc python -m pydoc
--- my_env/bin/activate.fish
+++ my_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/omar/Desktop/Desktop-omar/lia/test_env"
+set -gx VIRTUAL_ENV "/home/omar/Desktop/Desktop-omar/Sesame2-sim/my_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -56,7 +56,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(test_env) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(my_env) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -65,5 +65,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(test_env) "
+    set -gx VIRTUAL_ENV_PROMPT "(my_env) "
 end
